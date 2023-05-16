195aa731b1c6bce6f480a923e7f4847d675f4247
mas.sedighi <masoumeh.sedighii@gmail.com>
2023-05-16 15:05:36 +0330

    create blog and user model and usermanager

--- core/blog/models.py
+++ core/blog/models.py
@@ -2,3 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+class Blog(models.Model):
+    """
+    Blog model for app
+    """
+    title = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    catgory = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    content = models.TextField()
+    status = models.BooleanField
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+class Caregory(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
